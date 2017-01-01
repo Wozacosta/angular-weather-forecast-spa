b19d6c92ff3270c632c39133ed64f9cbe6aa8fff
Wozacosta <rabah.m.samy@gmail.com>
2017-01-01 22:25:37 +0100

    Routing setup

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-/**
- * Created by wozacosta on 1/1/17.
- */
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function($routeProvider){
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController'
+    })
+    .when('/forecast', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+    })
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope){
+
+}]);
